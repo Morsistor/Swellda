e3c6db6aa6bfa48e20fae37350bb15e1884bc71e
morsistor <maurice.grages@gmail.com>
2017-03-17 17:39:31 +0100

    Added first locations.

--- main.py
+++ main.py
@@ -3,27 +3,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index Page"
+    return render_template("index.html")
 
-@app.route("/hello/")
-def hello():
-    param1 = request.args.getlist('inventory')
-    print(param1)
-    print(request.args)
-    return "Inventory: {}".format(param1)
+@app.route("/castleyard")
+def yard():
+    return render_template("castleyard.html")
 
-@app.route("/user/<name>/")
-def helloname(name):
-    return "Hello " + name + ' !'
+@app.route("/castledun")
+def dun():
+    return render_template("castledun.html")
 
-@app.route("/templ/")
-@app.route("/templ/<var>")
-def template(var=None):
-    return render_template("templ.html", var=var)
+@app.route("/castlebed")
+def bed():
+    return render_template("castlebed.html")
 
-@app.route("/post/<int:num>/")
-def usernum(num):
-    return "This is post #{numb}".format (numb=num) + ". LOL random!"
+# @app.route("/hello/")
+# def hello():
+#     param1 = request.args.getlist('inventory')
+#     print(param1)
+#     print(request.args)
+#     return "Inventory: {}".format(param1)
+
+# @app.route("/user/<name>/")
+# def helloname(name):
+#     return "Hello " + name + ' !'
+
+# @app.route("/templ/")
+# @app.route("/templ/<var>")
+# def template(var=None):
+#     return render_template("templ.html", var=var)
+
+# @app.route("/post/<int:num>/")
+# def usernum(num):
+#     return "This is post #{numb}".format (numb=num) + ". LOL random!"
 
 if __name__ == "__main__":
     app.run()
