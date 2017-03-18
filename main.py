7c04b3564a217a62d56ae7102ebdde146a93b550
MarioHa <mario.hauer95@gmail.com>
2017-03-18 20:50:38 +0000

    Added inventory condition for 'hyrulegym'

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect
 app = Flask(__name__)
 
+
 # The Legend of Swellda
+
+
 
 @app.route("/")
 def start():
     return render_template("start.html")
 
+
 @app.route("/<location>")
 def loc(location):
     inv = request.args.getlist('inv')
-    
     inv_as_query = ""
     for item in inv:
         inv_as_query = inv_as_query + "inv=" + item + "&"
 
+    if location == 'hyrulegym' and 'barbell' not in inv:
+        return redirect(url_for('loc', location='nobarbell'))
+    
     return render_template(location + ".html", inv=inv, inv_as_query=inv_as_query)
-
-# Inventory
-# sword, shield, leather equipment, glass of milk, protein shake, barbell, sunglasses, Zelda's innocence, peanutbutter, steroids
 
 if __name__ == "__main__":
     app.run()
