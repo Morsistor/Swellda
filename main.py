c2d7f0edff4ee79b15707395b6dd3bcca2fdbd31
morsistor <maurice.grages@gmail.com>
2017-03-18 18:37:35 +0100

    Added base template and changed other templates accordingly. Also removed some useless code from main.py

--- main.py
+++ main.py
@@ -20,26 +20,5 @@ def loc(location):
 # Inventory
 # sword, shield, leather equipment, glass of milk, protein shake, barbell, sunglasses, Zelda's innocence, peanutbutter, steroids
 
-
-# @app.route("/hello/")
-# def hello():
-#     param1 = request.args.getlist('inv')
-#     print(param1)
-#     print(request.args)
-#     return "Inventory: {}".format(param1)
-
-# @app.route("/user/<name>/")
-# def helloname(name):
-#     return "Hello " + name + ' !'
-
-# @app.route("/templ/")
-# @app.route("/templ/<var>")
-# def template(var=None):
-#     return render_template("templ.html", var=var)
-
-# @app.route("/post/<int:num>/")
-# def usernum(num):
-#     return "This is post #{numb}".format (numb=num) + ". LOL random!"
-
 if __name__ == "__main__":
     app.run()
