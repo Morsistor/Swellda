9e68b920043a7210a377b389929cab5fdacd605a
morsistor <maurice.grages@gmail.com>
2017-03-18 16:26:50 +0100

    Added inventory and CSS

--- main.py
+++ main.py
@@ -7,21 +7,15 @@ app = Flask(__name__)
 def start():
     return render_template("start.html")
 
-@app.route("/hyrulemarket")
-def hyrulemarket():
-    return render_template("hyrulemarket.html")
+@app.route("/<location>")
+def loc(location):
+    inv = request.args.getlist('inv')
+    
+    inv_as_query = ""
+    for item in inv:
+        inv_as_query = inv_as_query + "inv=" + item + "&"
 
-@app.route("/castleyard")
-def yard():
-    return render_template("castleyard.html")
-
-@app.route("/castledun")
-def dun():
-    return render_template("castledun.html")
-
-@app.route("/castlebed")
-def bed():
-    return render_template("castlebed.html")
+    return render_template(location + ".html", inv=inv, inv_as_query=inv_as_query)
 
 # Inventory
 # sword, shield, leather equipment, glass of milk, protein shake, barbell, sunglasses, Zelda's innocence, peanutbutter, steroids
