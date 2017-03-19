507a62a5046fb543a3a0f140963ed915fd5ed981
morsistor <maurice.grages@gmail.com>
2017-03-19 15:30:58 +0100

    Built ad randomizer and on/off option

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def loc(location):
     inv = request.args.getlist('inv')
     name = request.args.getlist('name')
     gender = request.args.getlist('gender')
-    ads = request.args.getlist('ads')
+    ads = request.args.get('ads')
 
 
     inv_as_query = ""
@@ -30,10 +30,16 @@ def loc(location):
         if item != "Leather Mask":
             inv_as_query_minus_mask = inv_as_query_minus_mask + "inv=" + item + "&"
     
-    adnum = 3
-    adnum = random.sample(range(4), 1)
-    adnums = ""
-    adnums = str(adnum)
+    adnum = []
+    # adnum = random.randrange(2, 7, 3)
+    adnum = random.sample(range(2, 7), 2)
+    adnum1 = ""
+    adnum2 = ""
+    # for num in adnum:
+        # adnum[x] = str(adnum[x])
+    adnum1 = str(adnum[0])
+    adnum2 = str(adnum[1])
+
     # if ads == "yes":
 
     if location == 'hyrulegym' and 'Protein Shake' not in inv:
@@ -42,7 +48,7 @@ def loc(location):
     if location == 'shop' and 'Leather Mask' not in inv:
         return redirect(url_for('loc', location='shop2', inv=inv))
 
-    return render_template(location + ".html", inv=inv, inv_as_query=inv_as_query, inv_as_query_minus_mask=inv_as_query_minus_mask, adnums=adnums)
+    return render_template(location + ".html", inv=inv, inv_as_query=inv_as_query, inv_as_query_minus_mask=inv_as_query_minus_mask, adnum1=adnum1, adnum2=adnum2, ads=ads)
 
 if __name__ == "__main__":
     app.run()
